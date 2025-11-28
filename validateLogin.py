176c4e64ee92fd12d4c5785e3e9820d917e0d489
antonio <antonio.pulgarin97@gmail.com>
2025-11-28 09:52:20 -0500

    listo login

---
+++ validateLogin.py
@@ -0,0 +1,29 @@
+import csv
+
+def leerFile():
+    try:
+        with open("usuarios.csv", "r") as file:
+            reader = csv.DictReader(file)
+            return list(reader)
+    except:
+        print("error al leer archivo")
+        return []
+
+claves = leerFile()
+
+
+def validarLogin():
+    for i in range(3):
+        userName = input("user name: ")
+        userPassword = input("user password: ")
+
+        for user in claves:
+            if userName == user['user'] and userPassword == user['password']:
+                print(f"welcome: {userName}")
+                return True
+            else:
+                continue
+        print(f"usuario o contraseña incorrectos\nintentos restantes: {2 - i}")
+    print("excediste el limite de intentos")
+    return False
+
